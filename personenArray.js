76d0f08aa46c742d1ef19c7d3a930c2d4ef0dbd7
David Praschak <david.praschak@univie.ac.at>
2023-02-20 20:00:18 +0100

    Constructor finished, Class method Age created

--- personenArray.js
+++ personenArray.js
@@ -1,7 +1,12 @@
 class Person {
-    constructor(firstName, lastName, birthYear)
+    constructor(firstName, lastName, birthYear) {
     this.firstName = firstName;
     this.lastName = lastName 
     this.birthYear = birthYear
 }
+
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.birthYear;
+    }
 
