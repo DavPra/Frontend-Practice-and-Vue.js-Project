ea05f2a1fd14dc43d4f40009ddf722c93b30e55d
David Praschak <david.praschak@univie.ac.at>
2023-02-20 20:36:39 +0100

    Introduced various variables

--- personenArray.js
+++ personenArray.js
@@ -1,12 +1,25 @@
+let addPerson = document.getElementById(addPerson);
+let sortByLastName = document.getElementById(sortByLastName);
+let sortByAge = document.getElementById(sortByAge);
+let removeLast = document.getElementById(removeLast);
+let removeAll = document.getElementById(removeAll);
+
+
 class Person {
     constructor(firstName, lastName, birthYear) {
     this.firstName = firstName;
     this.lastName = lastName 
     this.birthYear = birthYear
+    }
+
 }
 
-    age() {
+
+age() {
         let date = new Date();
         return date.getFullYear() - this.birthYear;
     }
+
+const Personen = [];
+
 
