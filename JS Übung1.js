6ff9131ec0ebf5be29a69399aee5bab7005d8486
David Praschak <david.praschak@univie.ac.at>
2023-02-22 16:13:48 +0100

    Minor changes

--- JS Übung1.js
+++ JS Übung1.js
@@ -1,1 +1,2 @@
-document.getElementById(Clicks);
+let counter = document.getElementById('Clicks').innerText;
+document.getElementById('Clicker').addEventListener('click', function () {Clicks.innerText = parseInt(Clicks.innerText) + 1; })
