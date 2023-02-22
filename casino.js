9c93a4eccf255231665be567debd96d131115585
David Praschak <david.praschak@univie.ac.at>
2023-02-22 21:31:41 +0100

    casino.js edited

--- casino.js
+++ casino.js
@@ -1,6 +1,49 @@
 var money = 100;
 document.getElementById("money").innerHTML = money;
 
-function gamble() {
-  // TODO
+let wager
+wager = parseInt(document.getElementById("wager"))
+
+console.log(wager)
+
+let result
+result = document.getElementById("result")
+
+function checkWager () {
+
+
+if (wager > money) {
+  document.getElementById("result").innerText = "Versuche es nochmal mit einem kleineren Einsatz";
+}
+
+else if (wager < 1) {
+  document.getElementById("result").innerText = "Setze bitte mehr";
+}
+
+else {gamble}
+
+}
+
+function generatedNumber () {
+  Math.floor(Math.random() * 100)
+}
+function gamble () {
+if (generatedNumber < 48) {
+  result = "Rot"
+  console.log(result)
+} else if (generatedNumber < 96) {
+  result = "Schwarz"
+  console.log(result)
+} else {
+  result = "Grün"
+  console.log(result)
+}
+}
+
+
+if (document.getElementsByClassName("color").checked = result) {
+  money + (wager * 2)
+} else {
+    money - wager
+    
 }
