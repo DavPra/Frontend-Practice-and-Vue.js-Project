91903cf36e7a34b194b7357df10bdff61aea73d6
David Praschak <david.praschak@univie.ac.at>
2023-02-27 13:24:21 +0100

    plus button versuch

--- JS Übung1.js
+++ JS Übung1.js
@@ -1,2 +1,9 @@
 let counter = document.getElementById('Clicks').innerText;
-document.getElementById('Clicker').addEventListener('click', function () {Clicks.innerText = parseInt(Clicks.innerText) + 1; })
+document.getElementById('Clicker').addEventListener('click', function Clickser () {Clicks.innerText = parseInt(Clicks.innerText) + 1; })
+
+
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+let result = document.getElementById('result')
+
+document.getElementById('plus').addEventListener('click', function Plus () {result = parseInt(num1.innerText) + parseInt(num2.innerText);})
