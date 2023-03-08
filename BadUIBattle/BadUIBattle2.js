bf01fe16286f1b2a6e1cf749b74f234dc03d4732
David Praschak <david.praschak@univie.ac.at>
2023-03-08 17:53:57 +0100

    Timers corrected

--- BadUIBattle/BadUIBattle2.js
+++ BadUIBattle/BadUIBattle2.js
@@ -1,23 +1,23 @@
 let greeting = document.getElementById("greeting")
 
-setTimeout (greet, 500)
+setTimeout (greet, 5000)
 
 function greet () {
     document.getElementById("greeting").innerHTML = "Thank you for your time applicant. Please stand by while I test your aptitude and patience."
-    setTimeout (apTest, 500)
+    setTimeout (apTest, 5000)
 }
 
 function apTest () {
     document.getElementById("greeting").style.display = "none"
     document.getElementById("fastLane").style.display = ""
-    setTimeout (Test, 500)
+    setTimeout (Test, 5000)
 }
 
 document.getElementById("fastLane").addEventListener("click", skip)
 
 function skip () {
     document.getElementById("impatience").style.display =""
-    setTimeout (window.location.href = "BadUIBattle.html", 300);
+    setTimeout (window.location.href = "BadUIBattle.html", 3000);
 }
 
 function Test () {
