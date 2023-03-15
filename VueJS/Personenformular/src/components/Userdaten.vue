dfa5cc42dc7cdb8de14b19de292fc9c666098af6
David Praschak <david.praschak@univie.ac.at>
2023-03-15 20:35:23 +0100

    basics userdaten

--- VueJS/Personenformular/src/components/Userdaten.vue
+++ VueJS/Personenformular/src/components/Userdaten.vue
@@ -1,25 +1,11 @@
 <template> 
-    <div>
-        <ul>
-            <title>
-                Vorname
-            </title> <br>
-            <li>
-                <input type="text">
-            </li>
-            <title>
-                Nachname
-            </title> <br>
-            <li>
-                <input type="text">
-            </li>
-            <title>
-                Geburtsdatum
-            </title> <br>
-            <li>
-                <input type="text">
-            </li>
-        </ul>
-        <button>Speichern</button>
-    </div>
+
 </template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
