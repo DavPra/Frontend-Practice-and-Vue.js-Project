a7e114f49b43bee6c641f6c0585f07fbec8a5684
David Praschak <david.praschak@univie.ac.at>
2023-03-27 18:18:02 +0200

    SupermarktCard, Loginpage added

--- Abschluss Frontend/Abschlussprojekt Frontend/src/App.vue
+++ Abschluss Frontend/Abschlussprojekt Frontend/src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
 import ListCard from './components/ListCard.vue';
 import LoginPage from './components/LoginPage.vue';
-
-
+import SupermarktCard from './components/SupermarktCard.vue';
 </script>
+
+<template>
+
+<div>
+  <SupermarktCard/>
+
+</div> 
+
+</template>
---
+++ Abschluss Frontend/Abschlussprojekt Frontend/src/components/SupermarktCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card
+  title="Einkaufsliste Supermarkt"
+  subtitle="Subtitle"
+  text="..."
+  variant="tonal"
+>
+  <v-card-actions>
+    <v-btn>Bearbeiten</v-btn>
+    <v-btn>Löschen</v-btn>
+  </v-card-actions>
+</v-card>
+
+
+</template>
