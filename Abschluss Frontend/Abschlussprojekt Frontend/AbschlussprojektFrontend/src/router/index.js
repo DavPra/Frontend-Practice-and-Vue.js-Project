ab964b4cc80ba8d7c768becac9ef1a8d7d6e1b36
David Praschak <david.praschak@univie.ac.at>
2023-04-11 19:24:24 +0200

    Ordner fixed

--- Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/router/index.js
+++ Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/router/index.js
@@ -1,26 +1,13 @@
-// Composables
 import { createRouter, createWebHistory } from 'vue-router'
-
+import StartPageView from '@/views/StartPageView.vue'
+import LoginPageView from '@/views/LoginPageView.vue'
 const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
-  },
+{ path: '/', component: StartPageView },
+{ path: '/about', component: AboutView },
+{ path: '/login', component: LoginPageView}
 ]
-
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+routes,
+history: createWebHistory()
 })
-
-export default router
+export {router}
