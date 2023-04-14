64d065370b743c1858b24c4b94082dfd5152c359
David Praschak <david.praschak@univie.ac.at>
2023-04-14 20:34:07 +0200

    Buttons fixed partially

--- Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/components/ListForm.vue
+++ Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/components/ListForm.vue
@@ -1,43 +1,39 @@
 <script setup>
+  import { ref } from 'vue';
+  import { useItemStore } from '@/store/ItemStore';
+  import { defineEmits } from 'vue';
 
-import { ref } from 'vue';
-import { useItemStore } from '@/store/ItemStore';
+  const emit = defineEmits(['closeForm'])
 
-const ItemStore = useItemStore()
-const itemName = ref('')
+  const ItemStore = useItemStore();
+  const itemName = ref('');
 
-function addItem() {
+  function addItem() {
     ItemStore.Items.push ({
         name: itemName.value
-    })
-    itemName.value=''
+    });
+    itemName.value = '';
+  }
 
-}
+  function closeForm() {
+    emit('closeForm')
+    showForm.value = false
 
-
-
+  }
 </script>
 
 <template>
-
-        <v-card height="auto" width="300" title="Neuer Eintrag" style="margin-left: 20px;">
-            
-          <v-form @submit.prevent>
-            <v-text-field
-              v-model="itemName"
-              label="Neuer Eintrag"
-            ></v-text-field>
-            <v-btn type="submit" block class="mt-2" @click="addItem">Speichern</v-btn>
-          </v-form>
-            <ul style="margin-top: 20px;">
-            <li v-for="Items in ItemStore.Items">{{Items.name}}</li>
-        </ul> 
-        </v-card>
-
-
-
-
-
-
+  <v-card height="auto" width="300" title="Neuer Eintrag" style="margin-left: 20px;">
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="itemName"
+        label="Neuer Eintrag"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2" @click="addItem">Speichern</v-btn>
+      <v-btn type="submit" block class="mt-2" @click="closeForm">Abbrechen</v-btn>
+    </v-form>
+    <ul style="margin-top: 20px;">
+      <li v-for="Items in ItemStore.Items">{{Items.name}}</li>
+    </ul> 
+  </v-card>
 </template>
-    
--- Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/components/NewListButtonSM.vue
+++ Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/components/NewListButtonSM.vue
@@ -1,12 +1,19 @@
 <script setup>
-
+   import { ref } from 'vue';
+   import ListForm from './ListForm.vue';
    import { defineEmits } from 'vue';
 
    const emit = defineEmits(['newListSM']);
 
+   const showForm = ref(false);
+
    function newListSM() {
-   emit('newListSM');
+      emit('newListSM');
+      emit('showForm')
+      showForm.value = true;
    }
+
+
 
 
   </script>
@@ -20,5 +27,7 @@
     <v-icon icon=mdi-plus size="x-large"></v-icon>
     Neue Liste Supermarkt
    </v-btn>
+
+   <ListForm v-if="showForm" />
   </template>
   
