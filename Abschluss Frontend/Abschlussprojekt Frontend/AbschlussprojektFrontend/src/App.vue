9666f3aba62165bd885aa4ebc61440ceb0708166
David Praschak <david.praschak@univie.ac.at>
2023-04-14 14:14:09 +0200

    new buttons

--- Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/App.vue
+++ Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/App.vue
@@ -4,7 +4,7 @@ import LoginPage from './components/LoginPage.vue';
 import SupermarktCard from './components/SupermarktCard.vue';
 import TestCard from './components/TestCard.vue';
 import DrogerieCard from './components/DrogerieCard.vue';
-import NewListbutton from './components/NewListButton.vue';
+import NewListbuttonSM from './components/NewListButtonSM.vue';
 import TestView from './views/TestView.vue';
 import StartPageView from './views/StartPageView.vue';
 import ListForm from './components/ListForm.vue';
