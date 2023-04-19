d035e95e1e3b4c8f24aab0c3f9bb62e706ca36d6
David Praschak <david.praschak@univie.ac.at>
2023-04-19 19:46:02 +0200

    DRcard und SMcard updated

--- Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/components/DrogerieCard.vue
+++ Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/components/DrogerieCard.vue
@@ -2,8 +2,41 @@
 import { useItemStore } from '@/store/ItemStore';
 import { ref } from 'vue';
 
-const ItemStore = useItemStore();
-const currentDate = ref(new Date().toLocaleDateString())
+import axios from 'axios';
+
+  const ItemStore = useItemStore();
+  const currentDate = ref(new Date().toLocaleDateString())
+
+  function deleteList () {
+    const confirmed = confirm('Möchten Sie die Liste wirklich löschen?');
+    if (confirmed) {
+      axios.delete(`https://codersbay.a-scho-wurscht.at/api/tasklist/${ItemStore.taskID}`)
+        .then(response => {
+          if (response.status === 204) {
+            alert('Die Liste wurde erfolgreich gelöscht.');
+            ItemStore.removeItemList(ItemStore.taskID);
+          }
+        })
+        .catch(error => {
+          if(response.status === 401) {
+            alert('Bitte loggen Sie sich ein um die Listen zu bearbeiten.')
+          }
+          else if (response.status === 403) {
+            alert('Das ist nicht ihre Einkaufsliste.')
+          }
+          console.error('Error deleting list:', error);
+        });
+    }
+  }
+  const formOn = false
+
+  function editList () {
+
+      formOn = true
+  
+
+
+}
 
 
 </script>
@@ -22,8 +55,8 @@ const currentDate = ref(new Date().toLocaleDateString())
       </ul> 
       </v-card-text>
       <v-card-actions>
-        <v-btn>Bearbeiten</v-btn>
-        <v-btn>Löschen</v-btn>
+        <v-btn v-show="formOn" @click="editList">Bearbeiten</v-btn>
+        <v-btn @click="deleteList">Löschen</v-btn>
       </v-card-actions>
     </v-card>
   </template>
