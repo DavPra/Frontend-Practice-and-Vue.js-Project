8668640c88fdcb4cd9ac77bbc50ad3b545626bfb
David Praschak <david.praschak@univie.ac.at>
2023-04-24 10:56:47 +0200

    taskstore modified

--- Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/store/taskList.js
+++ Abschluss Frontend/Abschlussprojekt Frontend/AbschlussprojektFrontend/src/store/taskList.js
@@ -7,6 +7,7 @@ export const useTaskListStore = defineStore({
     taskListId: localStorage.getItem('taskListId') || '',
     listItems: [],
     answer: '',
+    taskId: localStorage.getItem('taskId') || '',
   }),
   actions: {
     setTaskListId(taskListId) {
@@ -43,5 +44,77 @@ export const useTaskListStore = defineStore({
     catch (error) {
       console.error(error);
     }
-  }
-}});
+  },
+  async createTask() {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+       errorMessage.value =
+         "Bitte loggen Sie sich ein um eine neue Liste zu erstellen.";
+        return;
+    }
+const config = { headers: { Authorization: `Bearer ${token}` } };
+
+  try {
+      const response = await axios.post('https://codersbay.a-scho-wurscht.at/api/task', {title: ''}, config);
+      if (response.status === 200) {
+        this.answer = response.data
+        console.log(response)
+        localStorage.setItem('taskId', response.data.taskId);
+      } else {
+        console.error('Fehler beim Erstellen der Aufgabe');
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  },
+  async deleteTask() {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+       errorMessage.value =
+         "Bitte loggen Sie sich ein um eine neue Liste zu erstellen.";
+        return;
+    }
+const config = { headers: { Authorization: `Bearer ${token}` } };
+
+
+  try {
+      const response = await axios.delete(`https://codersbay.a-scho-wurscht.at/api/task/${localStorage.getItem('taskId')}`, config);
+      if (response.status === 200) {
+        
+        console.log(response)
+
+        return 'Aufgabe gelöscht'
+      } else {
+        console.error('Fehler beim Löschen der Aufgabe');
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  },
+  async updateTask() {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+       errorMessage.value =
+         "Bitte loggen Sie sich ein um eine neue Liste zu erstellen.";
+        return;
+    }
+const config = { headers: { Authorization: `Bearer ${token}` } };
+
+    try {
+        const response = await axios.put(`https://codersbay.a-scho-wurscht.at/api/task/${localStorage.getItem('taskId')}`, {title: ''}, config);
+        if (response.status === 200) {
+          this.answer = response.data
+          console.log(response)
+        } else {
+          console.error('Fehler beim Updaten der Aufgabe');
+        }
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+}
+
+});
